df4f23a1695913481069df31d9bddd59bb2d48ae
Your Name <dev@example.com>
2026-07-26 17:48:59 +0000

    chore: initialize project with vite, react, and typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Navbar } from './components/Navbar/Navbar';
+import { Footer } from './components/Footer/Footer';
+import { Home } from './pages/Home/Home';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
